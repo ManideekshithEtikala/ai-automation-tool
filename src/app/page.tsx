c80caec9f3fefdce183048a0022b24d65900319f
manideekshith <manideekshithgaming@gmail.com>
2025-11-26 22:42:53 +0530

    inserted db page to activatethe prisma function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
-
-export default function Home() {
+import prisma from "@/lib/db";
+export default async function Home() {
+  const data= await prisma.user.findMany();
+  console.log(data);
   return (
    <div>hell0world</div>
   );

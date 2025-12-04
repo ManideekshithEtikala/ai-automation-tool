e83a7c4f2c17cd3340074bd2ab625acc846fc74e
manideekshith <manideekshithgaming@gmail.com>
2025-12-04 10:37:21 +0530

    added the background jobs, workflows and also multiterminal npm package mprocs to run multiple tabs at one time with dev:all command

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import { LogoutButton } from "./logout";
-import { requireAuth } from "@/lib/auth-utils";
-import {caller} from '@/trpc/server';
-const  Page=async ()=> {
-  await requireAuth();
-  const data = await caller.getUsers();
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+const  Page=() => {
+  const trpc=useTRPC();
+  const queryClient=useQueryClient();
+  const {data}=useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Workflow created successfully");
+  }}));
   return (
    <div className="min-h-screen min-w-screen flex items-center justify-center">
-{JSON.stringify(data)}
+{JSON.stringify(data,null,2)}
+<Button disabled={create.isPending} onClick={()=>create.mutate()}>Create Workflow</Button>
 <LogoutButton />
    </div>
   );
---
+++ src/inngest/functions.ts
@@ -0,0 +1,19 @@
+import { inngest } from "./client";
+import prisma from "@/lib/db";
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("fetching", "5s");
+    await step.sleep("transcribing", "5s");
+
+    await step.sleep("sending-to-ai", "5s");
+    await step.run("create-workflow", ()=>{
+        return prisma.workflows.create({
+            data:{
+              name:'Inngest Workflow'
+            }
+          }) 
+    });
+  },
+);
